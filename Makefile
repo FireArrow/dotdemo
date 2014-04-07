0a2daf17660faa9dd6a0787e9f03f7a456fd07e4
David <dersson@kth.se>
2014-04-07 12:47:27 +0200

    Now using ddhelpers. Removed some left over variables. Added minor sanity check for resolution

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
-SRC=main.c ddclientlib/ddclient.c
+SRC=main.c ddclientlib/ddclient.c ddclientlib/ddhelpers.c
 LIBS=-lSDL2_gfx
+PKG_CFG_LIBS=sdl2
 EXE=dotdemo
 CC=gcc
 
 all:
-	$(CC) $(SRC) -o $(EXE) -lm `pkg-config --libs --cflags sdl2` $(LIBS)
+	$(CC) $(SRC) -o $(EXE) -lm `pkg-config --libs --cflags $(PKG_CFG_LIBS)` $(LIBS)
 
 clean:
 	rm -f $(EXE)
--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <SDL2/SDL2_framerate.h>
 #include "ddclientlib/ddclient.h"
+#include "ddclientlib/ddhelpers.h"
 
 #define verboseOut(...) if(verbose) printf( __VA_ARGS__ )
 #define TRUE 1
@@ -20,8 +21,6 @@
 #define MAX_BALLS DD_MAX_DOTS * 10
 #endif
 
-static int pattern_size = 180;
-static int tri_offset = 30;
 static int screen_w = 0;
 static int screen_h = 0;
 static int wind_speed=5;
@@ -57,102 +56,6 @@ typedef struct Ball {
     int r,b,g;
     char keep;
 } Ball;
-
-void transformBase( float* v, const int from_dim, const int to_dim ) {
-    *v = ( *v * (float)to_dim ) / (float)from_dim;
-}
-
-void transformDots( float* laser_point_buf, int numberOfDots ) {
-    int i;
-    for( i=0; i<numberOfDots*2; i += 2 ) {
-        transformBase( &laser_point_buf[i], 640, screen_w ); // x
-        transformBase( &laser_point_buf[i+1], 480, screen_h ); // y
-    }
-}
-
-void setRect( SDL_Rect* rect, int x, int y, int w, int h ) {
-    rect->x = x;
-    rect->y = y;
-    rect->w = w;
-    rect->h = h;
-}
-
-void setPoly( Sint16* poly, size_t n, ... ) {
-    int i;
-    va_list values;
-    va_start( values, n );
-    for( i=0; i<n; ++i ) {
-        poly[i] = (Sint16) va_arg( values, int );
-    }
-    va_end( values );
-}
-
-void drawCalibrationPattern( SDL_Renderer* renderer ) {
-    static char first_run = 1;
-    int i, j;
-
-    static SDL_Rect rects[4];
-    static Sint16 triangles_x[4][3];
-    static Sint16 triangles_y[4][3];
-    if( first_run ) {
-        first_run = 0;
-        setRect( &rects[0], 0                      , 0                      , pattern_size, pattern_size );
-        setRect( &rects[1], screen_w - pattern_size, 0                      , pattern_size, pattern_size );
-        setRect( &rects[2], screen_w - pattern_size, screen_h - pattern_size, pattern_size, pattern_size );
-        setRect( &rects[3], 0                      , screen_h - pattern_size, pattern_size, pattern_size );
-
-
-        setPoly( &triangles_x[0][0], 12,
-                // Top left trinagle
-                tri_offset                              ,
-                pattern_size - tri_offset               ,
-                tri_offset                              ,
-
-                // Top right triangle
-                screen_w + tri_offset - pattern_size    ,
-                screen_w - tri_offset                   ,
-                screen_w - tri_offset                   ,
-
-                // Bottom right tringle
-                screen_w + tri_offset - pattern_size    ,
-                screen_w - tri_offset                   ,
-                screen_w - tri_offset                   ,
-
-                // Bottom left triangle
-                tri_offset                              ,
-                tri_offset                              ,
-                pattern_size - tri_offset               
-               );
-
-        setPoly( &triangles_y[0][0], 12,
-                // Top left trinagle
-                tri_offset                              ,
-                tri_offset                              ,
-                pattern_size - tri_offset               ,
-
-                // Top right triangle
-                tri_offset                              ,
-                tri_offset                              ,
-                pattern_size - tri_offset               ,
-
-                // Bottom right tringle
-                screen_h - tri_offset                   ,
-                screen_h + tri_offset - pattern_size    ,
-                screen_h - tri_offset                   ,
-
-                // Bottom left triangle
-                screen_h + tri_offset - pattern_size    ,
-                screen_h - tri_offset                   ,
-                screen_h - tri_offset
-               );
-    }
-
-    SDL_RenderFillRects( renderer, rects, 4 );
-    for( i=0; i<4; ++i ) {
-        filledPolygonRGBA( renderer, &triangles_x[i][0], &triangles_y[i][0], 3, 0x00, 0x00, 0x00, 0xFF );
-    }
-
-}
 
 void pruneDots(Dot* dotList){
 
@@ -359,8 +262,6 @@ int run( SDL_Window* window, SDL_Renderer* renderer, int ddclientfd ) {
     Parameters physicsParams;
 
     float laser_point_buf[DD_MAX_DOTS][2];
-
-    char netByf[DD_SEND_BUF_SIZE];
 
     SDL_Event event;
 
@@ -620,7 +521,12 @@ int main( int argc, char** argv ) {
         screen_w = display_mode.w;
         screen_h = display_mode.h;
     }
+    if( screen_w < 100 || screen_h < 100 ) {
+        fprintf( stderr, "Warning: You are using a silly low resolution!\n" );
+    }
     verboseOut( "Using resolution %dx%d\n", screen_w, screen_h );
+
+    initDDhelpers( screen_w, screen_h, fmin( screen_w, screen_h ) / 4, 10 );
 
     verboseOut( "Creating window\n" );
     window = SDL_CreateWindow( "Dotdetector demo",
